0e6168ed320964ed73bdea957c9cbf90a6c55bc7
anjallawaju <anjallawaju0@gmail.com>
2021-07-21 12:04:09 -0500

    commit

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
+import { useContext } from 'react';
+import FavoriteContext from '../../store/favorites-context';
+
+
+function MainNavigation() {
+
+    const favoriteCtx = useContext(FavoriteContext);
+return (
+<header className={classes.header}>
+    <div className={classes.logo}> React Meetups</div>
+    <nav>
+        <ul>
+            <li>
+                <Link to='/'>All Meetups</Link>
+            </li>
+              <li> 
+                 <Link to='/new-meetup'>Add New Meetup</Link>
+            </li>
+            <li>
+                <Link to='/favorites'>
+                    My Favorites
+                    <span className={classes.badge}>{favoriteCtx.totalFavorites}
+                    </span>
+                    </Link>
+            </li>
+        </ul>
+    </nav>
+</header>
+);
+}
+
+
+export default MainNavigation;
